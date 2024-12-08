cf1135df62ac341df46f1a19efe8f8dd8135d9ce
chayon1001 <baust.cse.200201001@gmail.com>
2024-12-08 16:47:56 +0600

    added the package simple awasome reveal

--- src/components/High/HighestRatedGames.jsx
+++ src/components/High/HighestRatedGames.jsx
@@ -1,15 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
+import { Fade } from "react-awesome-reveal";
+
+import './HighestRated.css'
+
 const HighestRatedGames = () => {
     const [games, setGames] = useState([]);
 
-    
+
     useEffect(() => {
-        fetch("http://localhost:5000/review") 
+        fetch("http://localhost:5000/review")
             .then((res) => res.json())
             .then((data) => {
-               
+
                 const highestRatedGames = data
                     .sort((a, b) => b.rating - a.rating)
                     .slice(0, 6);
@@ -19,51 +23,57 @@ const HighestRatedGames = () => {
     }, []);
 
     return (
-        <div className=" py-10 ">
+        <div className=" py-10 highest-rated-games">
             <div className="container mx-auto px-4">
-                <h2 className="text-3xl font-semibold text-white mb-8 text-center">
-                    Highest Rated Games
-                </h2>
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-                    {
-                        games.map((game) => (
-                            <div
-                                key={game._id}
-                                className=" rounded-lg  border-2 bg-gray-300">
+                <Fade duration={1000}>
+                    <h2 className="text-3xl font-semibold text-white mb-8 text-center">
+                        Highest Rated Games
+                    </h2>
+                </Fade>
 
-                                <div className="p-4">
-                                    <img
-                                        src={game.coverImage}
+                <Fade delay={500} duration={1000}>
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+                        {
+                            games.map((game) => (
+                                <div
+                                    key={game._id}
+                                    className=" rounded-lg  border-2 bg-gray-300">
 
-                                        className="w-full h-52   rounded-md" />
+                                    <div className="p-4">
+                                        <img
+                                            src={game.coverImage}
+
+                                            className="w-full h-52   rounded-md" />
+                                    </div>
+                                    <div className="p-4">
+
+                                        <h3 className="text-xl font-semibold text-black mb-2">
+                                            {game.gameTitle}
+                                        </h3>
+
+
+                                        <p className="text-black mb-4">Rating :  {game.rating}/10
+
+                                        </p>
+
+
+                                        <p className="text-black mb-4"> Year : {game.publishingYear}
+
+                                        </p>
+
+
+                                        <Link
+                                            to={`/review/${game._id}`}
+                                            className="block text-center bg-yellow-500 text-black py-2 px-4 rounded-md font-semibold "
+                                        >
+                                            Explore Details
+                                        </Link>
+                                    </div>
                                 </div>
-                                <div className="p-4">
+                            ))}
+                    </div>
 
-                                    <h3 className="text-xl font-semibold text-black mb-2">
-                                        {game.gameTitle}
-                                    </h3>
-
-
-                                    <p className="text-black mb-4">Rating :  {game.rating}/10
-
-                                    </p>
-
-
-                                    <p className="text-black mb-4"> Year : {game.publishingYear}
-
-                                    </p>
-
-                                   
-                                    <Link
-                                        to={`/review/${game._id}`}
-                                        className="block text-center bg-yellow-500 text-black py-2 px-4 rounded-md font-semibold "
-                                    >
-                                        Explore Details
-                                    </Link>
-                                </div>
-                            </div>
-                        ))}
-                </div>
+                </Fade>
             </div>
         </div>
     );
